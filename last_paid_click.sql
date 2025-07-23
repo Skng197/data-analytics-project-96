6c2db5af4124ace725c0ccba8edd7f049afc632f
E2E4 <skng197@gmail.com>
2025-07-23 15:12:19 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,14 @@
-select
+SELECT
     t.visitor_id,
-    TO_CHAR(t.visit_date, 'DD.MM.YYYY') AS visit_date,
     t.utm_source,
     t.utm_medium,
     t.utm_campaign,
     t.lead_id,
-    TO_CHAR(t.created_at, 'DD.MM.YYYY') AS created_at,
     t.amount,
     t.closing_reason,
-    t.status_id
+    t.status_id,
+    TO_CHAR(t.visit_date, 'DD.MM.YYYY') AS visit_date,
+    TO_CHAR(t.created_at, 'DD.MM.YYYY') AS created_at
 FROM (
     SELECT
         l.visitor_id,
@@ -25,12 +25,15 @@ FROM (
             PARTITION BY l.lead_id
             ORDER BY s.visit_date DESC
         ) AS rn
-    FROM public.leads l
-    JOIN public.sessions s
-      ON l.visitor_id = s.visitor_id
-     AND s.visit_date < l.created_at
-     AND LOWER(s.medium) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-) t
+    FROM public.leads AS l
+    INNER JOIN public.sessions AS s
+        ON
+            l.visitor_id = s.visitor_id
+            AND l.created_at > s.visit_date
+            AND LOWER(s.medium) IN (
+                'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
+            )
+) AS t
 WHERE t.rn = 1
 ORDER BY
     t.amount DESC NULLS LAST,
