3c036c0cff99c01d82e8143516bde64f02aa2bf2
E2E4 <skng197@gmail.com>
2025-07-23 15:26:54 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,14 @@
 SELECT
     t.visitor_id,
+    TO_CHAR(t.visit_date, 'DD.MM.YYYY') AS visit_date,
     t.utm_source,
     t.utm_medium,
     t.utm_campaign,
     t.lead_id,
+    TO_CHAR(t.created_at, 'DD.MM.YYYY') AS created_at,
     t.amount,
     t.closing_reason,
-    t.status_id,
-    TO_CHAR(t.visit_date, 'DD.MM.YYYY') AS visit_date,
-    TO_CHAR(t.created_at, 'DD.MM.YYYY') AS created_at
+    t.status_id
 FROM (
     SELECT
         l.visitor_id,
@@ -22,17 +22,14 @@ FROM (
         l.closing_reason,
         l.status_id,
         ROW_NUMBER() OVER (
-            PARTITION BY l.lead_id
+            PARTITION BY l.visitor_id  -- Исправлено с lead_id на visitor_id
             ORDER BY s.visit_date DESC
         ) AS rn
     FROM public.leads AS l
-    INNER JOIN public.sessions AS s
-        ON
-            l.visitor_id = s.visitor_id
-            AND l.created_at > s.visit_date
-            AND LOWER(s.medium) IN (
-                'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
-            )
+    JOIN public.sessions AS s
+        ON l.visitor_id = s.visitor_id
+        AND s.visit_date <= l.created_at  -- Добавлено = для включения одновременных визитов
+        AND LOWER(s.medium) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 ) AS t
 WHERE t.rn = 1
 ORDER BY
