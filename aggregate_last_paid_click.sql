30a2c3b8e9f908002c764297144f9a00414494f8
E2E4 <skng197@gmail.com>
2025-07-24 16:44:38 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,6 +1,6 @@
 WITH last_paid_clicks AS (
-    SELECT DISTINCT
-        s.visitor_id,
+    SELECT
+        l.visitor_id,
         s.visit_date::date AS visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
@@ -8,19 +8,16 @@ WITH last_paid_clicks AS (
         l.lead_id,
         l.amount,
         l.closing_reason,
-        l.status_id
-    FROM public.sessions s
-    INNER JOIN public.leads l ON s.visitor_id = l.visitor_id
-    WHERE s.visit_date <= l.created_at
-    AND LOWER(COALESCE(s.medium, '')) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    AND NOT EXISTS (
-        SELECT 1
-        FROM public.sessions s2
-        WHERE s2.visitor_id = s.visitor_id
-        AND s2.visit_date > s.visit_date
-        AND s2.visit_date <= l.created_at
-        AND LOWER(COALESCE(s2.medium, '')) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    )
+        l.status_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.lead_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM public.leads l
+    JOIN public.sessions s
+        ON l.visitor_id = s.visitor_id
+        AND s.visit_date < l.created_at
+        AND LOWER(COALESCE(s.medium, '')) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 ),
 ads_costs AS (
     SELECT
@@ -65,6 +62,7 @@ LEFT JOIN ads_costs ac ON
     lpc.utm_source = ac.utm_source AND
     lpc.utm_medium = ac.utm_medium AND
     lpc.utm_campaign = ac.utm_campaign
+WHERE lpc.rn = 1
 GROUP BY 
     lpc.visit_date,
     lpc.utm_source,
@@ -72,9 +70,12 @@ GROUP BY
     lpc.utm_campaign,
     ac.daily_cost
 ORDER BY
-    revenue DESC NULLS LAST,
+    COALESCE(SUM(CASE 
+          WHEN lpc.closing_reason = 'Успешно реализовано' OR lpc.status_id = 142 
+          THEN lpc.amount 
+          ELSE 0 
+        END), 0) DESC NULLS LAST,
     lpc.visit_date ASC,
-    visitors_count DESC,
     lpc.utm_source ASC,
     lpc.utm_medium ASC,
     lpc.utm_campaign ASC
