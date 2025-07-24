0cf92d0e1d2e47cd0d138d241d5e2572e0251743
E2E4 <skng197@gmail.com>
2025-07-24 18:07:53 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,6 +1,6 @@
 WITH last_paid_clicks AS (
     SELECT
-        s.visitor_id,
+        l.visitor_id,
         s.visit_date::date AS visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
@@ -13,10 +13,16 @@ WITH last_paid_clicks AS (
             PARTITION BY l.lead_id
             ORDER BY s.visit_date DESC
         ) AS rn
-    FROM public.sessions s
-    INNER JOIN public.leads l ON s.visitor_id = l.visitor_id
-    WHERE s.visit_date < l.created_at
-    AND LOWER(COALESCE(s.medium, '')) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    FROM public.leads l
+    JOIN public.sessions s
+        ON l.visitor_id = s.visitor_id
+        AND s.visit_date < l.created_at
+        AND LOWER(COALESCE(s.medium, '')) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+filtered_last_paid_clicks AS (
+    SELECT *
+    FROM last_paid_clicks
+    WHERE rn = 1
 ),
 ads_costs AS (
     SELECT
@@ -39,39 +45,38 @@ ads_costs AS (
     GROUP BY utm_source, utm_medium, utm_campaign, campaign_date::date
 )
 SELECT
-    lpc.visit_date,
-    COUNT(DISTINCT lpc.visitor_id) AS visitors_count,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
+    flpc.visit_date,
+    COUNT(DISTINCT flpc.visitor_id) AS visitors_count,
+    flpc.utm_source,
+    flpc.utm_medium,
+    flpc.utm_campaign,
     COALESCE(ac.daily_cost, 0) AS total_cost,
-    COUNT(DISTINCT lpc.lead_id) AS leads_count,
+    COUNT(DISTINCT flpc.lead_id) AS leads_count,
     COUNT(DISTINCT CASE 
-                      WHEN lpc.closing_reason = 'Успешно реализовано' OR lpc.status_id = 142 
-                      THEN lpc.lead_id 
+                      WHEN flpc.closing_reason = 'Успешно реализовано' OR flpc.status_id = 142 
+                      THEN flpc.lead_id 
                     END) AS purchases_count,
     COALESCE(SUM(CASE 
-          WHEN lpc.closing_reason = 'Успешно реализовано' OR lpc.status_id = 142 
-          THEN lpc.amount 
+          WHEN flpc.closing_reason = 'Успешно реализовано' OR flpc.status_id = 142 
+          THEN flpc.amount 
           ELSE 0 
         END), 0) AS revenue
-FROM last_paid_clicks lpc
+FROM filtered_last_paid_clicks flpc
 LEFT JOIN ads_costs ac ON 
-    lpc.visit_date = ac.cost_date AND
-    lpc.utm_source = ac.utm_source AND
-    lpc.utm_medium = ac.utm_medium AND
-    lpc.utm_campaign = ac.utm_campaign
-WHERE lpc.rn = 1
+    flpc.visit_date = ac.cost_date AND
+    flpc.utm_source = ac.utm_source AND
+    flpc.utm_medium = ac.utm_medium AND
+    flpc.utm_campaign = ac.utm_campaign
 GROUP BY 
-    lpc.visit_date,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
+    flpc.visit_date,
+    flpc.utm_source,
+    flpc.utm_medium,
+    flpc.utm_campaign,
     ac.daily_cost
 ORDER BY
     revenue DESC NULLS LAST,
-    lpc.visit_date ASC,
-    lpc.utm_source ASC,
-    lpc.utm_medium ASC,
-    lpc.utm_campaign ASC
+    flpc.visit_date ASC,
+    flpc.utm_source ASC,
+    flpc.utm_medium ASC,
+    flpc.utm_campaign ASC
 LIMIT 15;
