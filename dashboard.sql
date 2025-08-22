1de2010428845488e4923a698d6bc5b1a9a554dd
E2E4 <skng197@gmail.com>
2025-08-22 13:23:29 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -1,34 +1,25 @@
+
 --- метрики
 
---- CPU  Cost Per User Стоимость привлечения одного пользователя
-SUM(total_cost) / NULLIF(SUM(visitors_count), 0);
----
+--- CPU Cost Per User Стоимость привлечения одного пользователя
+SELECT SUM(total_cost) / NULLIF(SUM(visitors_count), 0);
 
 --- ROI Return on Investment Окупаемость инвестиций
-(COALESCE(SUM(revenue), 0) - COALESCE(SUM(total_cost), 0))
-/ NULLIF(COALESCE(SUM(total_cost), 0), 0)
----
+SELECT (COALESCE(SUM(revenue), 0) - COALESCE(SUM(total_cost), 0))
+       / NULLIF(COALESCE(SUM(total_cost), 0), 0);
 
 --- CPPU Cost Per Paying User Стоимость привлечения одного платящего пользователя
-SUM(total_cost) / NULLIF(SUM(purchases_count), 0)
----
+SELECT SUM(total_cost) / NULLIF(SUM(purchases_count), 0);
 
 --- CPL Cost Per Lead Стоимость одного лида
-SUM(total_cost) / NULLIF(SUM(visitors_count), 0)
----
+SELECT SUM(total_cost) / NULLIF(SUM(visitors_count), 0);
 
 --- CAC Customer Acquisition Cost Стоимость привлечения клиента
-SUM(CASE WHEN "leads_count" > 0 THEN "total_cost" ELSE 0 END) 
-/ 
-NULLIF(SUM(CASE WHEN "leads_count" > 0 THEN "leads_count" ELSE 0 END), 0)
----
+SELECT SUM(CASE WHEN "leads_count" > 0 THEN "total_cost" ELSE 0 END) 
+       / NULLIF(SUM(CASE WHEN "leads_count" > 0 THEN "leads_count" ELSE 0 END), 0);
 
----Conversion Rate СК
-SUM(purchases_count) / SUM(leads_count)
----
+--- Conversion Rate СК
+SELECT SUM(purchases_count) / NULLIF(SUM(leads_count), 0);
 
----Roi Percent
-(SUM(revenue) - SUM(total_cost)) / NULLIF(SUM(total_cost), 0) * 100
----
-
-
+--- Roi Percent
+SELECT (SUM(revenue) - SUM(total_cost)) / NULLIF(SUM(total_cost), 0) * 100;
