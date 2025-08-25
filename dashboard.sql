be8a87ff7ed2a538bc8b07430f741fc7f9edb247
E2E4 <skng197@gmail.com>
2025-08-25 14:44:41 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -1,3 +1,48 @@
+-- last_paid_click
+
+WITH joined_data AS (
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.lead_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM sessions AS s
+    LEFT JOIN leads AS l
+        ON
+            s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE
+        LOWER(s.medium) IN (
+            'cpc', 'cpm', 'cpp', 'cpa', 'youtube', 'tg', 'social'
+        )
+)
+
+SELECT
+    jd.visitor_id,
+    jd.visit_date,
+    jd.utm_source,
+    jd.utm_medium,
+    jd.utm_campaign,
+    jd.lead_id,
+    jd.created_at,
+    jd.amount,
+    jd.closing_reason,
+    jd.status_id
+FROM joined_data AS jd
+WHERE jd.rn = 1 OR jd.lead_id IS NULL
+ORDER BY jd.amount DESC NULLS LAST, jd.visit_date ASC
+LIMIT 10;
+
 --- метрики
 
 --- CPU Cost Per User Стоимость привлечения одного пользователя
